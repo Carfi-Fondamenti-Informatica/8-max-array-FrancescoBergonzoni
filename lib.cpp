f34fa6f362615d4e3b7382b54072556345fce159
FrancescoBergonzoni <91878700+FrancescoBergonzoni@users.noreply.github.com>
2021-12-11 17:47:18 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,7 +1,7 @@
 
 #include "lib.h"
 
-float funzione(const float numeri[], int dim){
+float funzione(float numeri[], int dim){
    numeri[dim];
    float massimo = numeri[0];
    for(int i=0 ; i<dim ; i++){
