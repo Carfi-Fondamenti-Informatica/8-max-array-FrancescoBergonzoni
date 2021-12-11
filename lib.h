8ca05e01b91727dacb9f2fda45af7ea424f255d4
FrancescoBergonzoni <91878700+FrancescoBergonzoni@users.noreply.github.com>
2021-12-11 16:40:12 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,6 +1,6 @@
 #ifndef UNTITLED8_LIB_H
 #define UNTITLED8_LIB_H
-
+float funzione(float numeri[], int dimensione);
 #endif //UNTITLED8_LIB_H
 
-float funzione(int dimensione, float numeri[dimensione]);
+
