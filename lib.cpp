6ab77a7f38231ff411b428841107786d1c658b79
FrancescoBergonzoni <91878700+FrancescoBergonzoni@users.noreply.github.com>
2021-12-11 16:54:12 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -3,11 +3,11 @@
 
 float funzione(const float numeri[], int dim){
    numeri[dim];
-   float max=numeri[0];
+   float massimo = numeri[0];
    for(int i=0 ; i<dim ; i++){
-       if(numeri[i]>max){
-           max=numeri[i];
+       if(massimo < numeri[i]){
+           massimo = numeri[i];
        }
    }
-   return max;
+   return massimo;
 }
