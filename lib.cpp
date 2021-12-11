878e0b88cba2807239ca78fdcf88ce6e9c555db8
FrancescoBergonzoni <91878700+FrancescoBergonzoni@users.noreply.github.com>
2021-12-11 16:34:55 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include "lib.h"
+using namespace std;
 
+float funzione(const int dimensione, float numeri[dimensione]){
+   numeri[dimensione];
+   float max=num[0];
+   for(int i=0, i<dimensione, i++){
+       if(numeri[1]>max){
+           max=numeri[1];
+       }
+   }
+   return max;
+}
